a418b970713dd929d44853c07da927c96a05dc7d
madpo <madpo.s.a@gmail.com>
2015-04-14 00:27:43 +0300

    Style edit

--- rusrate.php
+++ rusrate.php
@@ -4,7 +4,7 @@
 # @license		GNU/GPL v2.0
 # Updated		11st April 2015
 #
-# Based on Elcora's StopKids ( http://elcora.com ), under GNU/GPL v2.0
+# Based on Ecolora's StopKids ( http://ecolora.com ), under GNU/GPL v2.0
 #
 # Site: http://aunited.ru
 # Email: info@aunited.ru
@@ -43,48 +43,69 @@ class  plgSystemRusRate extends JPlugin {
         JPlugin::loadLanguage('plg_system_rusrate', JPATH_ADMINISTRATOR);
 		
 		//Getting params
- 	    $position = $this->params->get('position', 'tl');
-   		$colored=$this->params->get('colored', 1);    
+ 	    $position = $this->params->get('position', 'tl');    
 		$age = $this->params->get('age', 18);
-   		$zindex = $this->params->get('zindex', 800);
-   		$color = $this->params->get('color', '#FFFFFF');
-   		$bgcolor = $this->params->get('bgcolor', '#BC121F');
-		$text='';
-		if($colored){
-			if ($age<12) $bgcolor='green';
-			if ($age<=16) $bgcolor='yellow';
-			if ($age>16) $bgcolor='red';
-		}
-		switch($age) {
-			case '0': $text='PLG_SYSTEM_RUSRATE_MESSAGE_ZERO'; break;
-			case '18': case '21': $text='PLG_SYSTEM_RUSRATE_MESSAGE_ADULT'; break;
-			default: $text='RESTRICTED_FOR'.$age.'YEARS_OLD'; break;
+		if($age > 12){
+				$text = JText::sprintf('PLG_SYSTEM_RUSRATE_WARN_MESSAGE', $age);
+		}else{
+				$text = JText::_('PLG_SYSTEM_RUSRATE_SEC_MESSAGE');
 		}
 
 		// Getting created page text
 		$buffer = JResponse::getBody();
 		// Making replacements
-		$buffer = str_replace('</body>', '<div id="rr'.$position.'" style=""><div class="rrage'.$position.'">'.$age.'+<div class="rrtip'.$position.'">'.text.'.</div></div></div></body>', $buffer);
+		$buffer = str_replace('</body>', '<div class="rr p_'.$position.'"><div class="shape"></div><p class="rrage">'.$age.'+</p><p class="rrmessage">'.$text.'.</p></div></body>', $buffer);
 
 		if ($buffer != '') {
 			// Moving page text
 			JResponse::setBody($buffer);
 		}
-		$document->addStyleSheet(JURI::root(true).'/plugins/system/rusrate/css/rusrate.css');
-         
-        $document->addStyleDeclaration('
-			#rr, #rrbr, #rrbl, #rrtl, #rrtr {
-				background-color: '.$bgcolor.' !important
-				z-index: '.$zindex.';
-            }
-			.rrage, .rragetl, .rragetr, .rragebl, .rragebr{
-				color: '.$color.';
-			}
-            .rrtip, .rrtiptl, .rrtiptr, .rrtipbl, .rrtipbr {
-				background-color: '.$bgcolor.' !important;
-				color: '.$color.';
-            }');
 		return true;
+	}
+	
+	function onAfterRoute()	{
+			$app = JFactory::getApplication();
+		    // check if we are not in the admin panel
+		    if ($app->getName()!= 'site') {
+			  return true;
+	     	}
+
+			$document = JFactory::getDocument();
+            if ($document->getType() !== 'html') return true;
+
+            if (JRequest::getVar('tmpl') === 'component') return true;
+
+            if (plgSystemRusRate::ShowOnFP()) return true;
+			$document->addStyleSheet(JURI::root(true).'/plugins/system/rusrate/css/rusrate.css');
+			//Getting params
+			$position = $this->params->get('position', 'tl');  
+			$age = $this->params->get('age', 18);
+			$zindex = $this->params->get('zindex', 800);
+			$color = $this->params->get('color', '#FFFFFF');
+			$bgcolor = $this->params->get('bgcolor', '#FF0000');
+			$colored = $this->params->get('colored', '1');
+			$details = $this->params->get('details', 1);
+			if($colored){
+			if ($age<12) $bgcolor='YellowGreen';
+			if (($age>=12)and($age<=16)) $bgcolor='orange';
+			if ($age>16) $bgcolor='FireBrick';
+			}
+			if(!$details){
+				$dStyle = '.rrmessage{display:none !important;}';
+			}else{
+				$dStyle = '.rr:hover .rrage, .rr:hover .shape{display:none;}.rr:hover .rrmessage{display:block;}';
+			}
+			$style = '
+				.p_br .shape, .p_bl .shape{ border-bottom: 75px solid '.$bgcolor.' !important; }
+				.p_tr .shape, .p_tl .shape{ border-top: 75px solid '.$bgcolor.' !important; }
+				.rrmessage{background-color:'.$bgcolor.';}
+				.rr a{color:'.$color.' !important;}
+				.rrmessage{color:'.$color.';}
+				.rrage{color:'.$color.';}
+				.rr {z-index:'.$zindex.';}
+				.rrage{z-index:'.($zindex + 1).';}
+			';
+			$document->addStyleDeclaration($style.$dStyle);
 	}
 
 		function ShowOnFP() {
